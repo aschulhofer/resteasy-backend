390a54a18dd78e73fa19ff69fe7b28d28f3052e4
aschulhofer <andreas.schulhofer@gmail.com>
2016-06-13 20:19:31 +0200

    Added cors headers

--- src/main/java/at/woodstick/service/HelloWorldRestService.java
+++ src/main/java/at/woodstick/service/HelloWorldRestService.java
@@ -14,7 +14,14 @@ public class HelloWorldRestService {
 	@Path("helloworld")
 	@Produces("application/json")
 	public Response getHelloWorld() {
-		return Response.ok(new RestMessage("Hello World")).build();
+		return Response
+				.ok(new RestMessage("Hello World - cross origin"))
+				.header("Access-Control-Allow-Origin", "*")
+	            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+	            .header("Access-Control-Allow-Credentials", "true")
+	            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+	            .header("Access-Control-Max-Age", "1209600")
+				.build();
 	}
 	
 }
